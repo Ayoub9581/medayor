630d97d2a85f282622313dc228a7e26466e283de
Ayoub Ar <ayoubarahmat9@gmail.com>
2019-01-02 15:06:59 +0100

    urls change

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.index, name='home'),
     path('', views.launch, name='launch'),
-    # path('projects/create', views.create_project, name='createproject'),
+    path('home/', views.index, name='home'),
     path('home/projects/', views.projects, name='projects'),
 ]
