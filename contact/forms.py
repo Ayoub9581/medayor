c29a61e5bc9e6fd0970b2dd9083a0d692a382a7d
Ayoub Ar <ayoubarahmat9@gmail.com>
2019-01-02 19:40:37 +0100

    email

--- contact/forms.py
+++ contact/forms.py
@@ -8,7 +8,8 @@ class ContactForm(forms.ModelForm):
         fields = ('name','email','note',)
         widgets = {
         'name':forms.TextInput(attrs={'class':'form-control','type':'text'}),
-        'email':forms.TextInput(attrs={'class':'form-control','type':'text'}),
+        'email':forms.TextInput(attrs={'class':'form-control','type':'email','required':'',
+        ' aria-required':'True'}),
         'note':forms.Textarea(attrs={'class':'form-control no-resize',
         'placeholder':'envoyez-nous un message.. ',
         'rows':'4'}),
