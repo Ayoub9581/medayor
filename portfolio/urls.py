ba51f4da8c9a5d19a7778a158356fe8c62314ef5
Ayoub Ar <ayoubarahmat9@gmail.com>
2019-01-08 18:06:04 +0100

    home

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.launch, name='launch'),
-    path('home/', views.index, name='home'),
+    path('', views.index, name='home'),
     path('projects/',views.projects, name='projects'),
 ]
